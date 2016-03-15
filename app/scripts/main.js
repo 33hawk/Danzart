7cdd3a593a66358f1a02f25e9546f89f21dc2e11
Daniel Godigna <danielgodigna@outlook.com>
2016-03-15 01:41:52 +0100

    Added teacher(in croatian), added info to attire, to top button home & about, picture changed in llet(silabus), JS conosole, deletead roses css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,26 @@
+// Avoid `console` errors in browsers that lack a console.
+( function() {
+    var method;
+    var noop = function() {};
+    var methods = [
+        "assert", "clear", "count", "debug", "dir", "dirxml", "error",
+        "exception", "group", "groupCollapsed", "groupEnd", "info", "log",
+        "markTimeline", "profile", "profileEnd", "table", "time", "timeEnd",
+        "timeline", "timelineEnd", "timeStamp", "trace", "warn"
+    ];
+    var length = methods.length;
+    var console = ( window.console = window.console || {} );
+
+    while ( length-- ) {
+        method = methods[ length ];
+
+        // Only stub undefined methods.
+        if ( !console[ method ] ) {
+            console[ method ] = noop;
+        }
+    }
+}() );
+
 //Header
 $( function() {
 
@@ -126,7 +149,7 @@ $back_to_top.on( "click", function( event ) {
 
 } );
 
-//Table
+//Schedules
 var headertext = [],
 headers = document.querySelectorAll( "#schedules th" ),
 tablerows = document.querySelectorAll( "#schedules th" ),
