b8d4330fd268ee729c6aee4c7ed6c395069585a4
Daniel Godigna <danielgodigna@gmail.com>
2016-09-02 04:24:10 +0200

    Project changed to gulp.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
+(function () {
   var method;
-  var noop = function () {};
+  var noop = function () { };
   var methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
@@ -14,28 +14,28 @@
   while (length--) {
     method = methods[length];
 
-        // Only stub undefined methods.
+    // Only stub undefined methods.
     if (!console[method]) {
       console[method] = noop;
     }
   }
-}());
+} ());
 //HTML5 Video Android phones
 function myOnload() {
   var ua = navigator.userAgent;
   if (typeof ua != 'undefined' && ua.match(/Android.* 2\.[0-2](\.[0-9]+)*[^.0-9]/i) !== null) {
     var vid = document.getElementById('video');
-    vid.addEventListener('click', function() {
+    vid.addEventListener('click', function () {
       vid.play();
     }, false);
   }
 }
 window.onload = myOnload;
 //header
-$(function() {
+$(function () {
   function mobilecheck() {
     var check = false;
-    (function(a) {
+    (function (a) {
       if (/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) {
         check = true;
       }
@@ -55,7 +55,7 @@ $(function() {
     $(items).removeClass('open').addClass('close');
   }
 
-  $('#navToggle').on(clickevent, function() {
+  $('#navToggle').on(clickevent, function () {
     event.stopPropagation();
     event.preventDefault();
     if (content.hasClass('open')) {
@@ -64,7 +64,7 @@ $(function() {
       open();
     }
   });
-  content.click(function() {
+  content.click(function () {
     if (content.hasClass('open')) {
       close();
     }
@@ -73,25 +73,25 @@ $(function() {
 });
 
 //Smooth scrolling
-$( function() {
+$(function () {
 
-  $( 'a[href*=#]:not([href=#])' ).click( function( e ) {
+  $('a[href*=#]:not([href=#])').click(function (e) {
 
     e.preventDefault(); {
 
-      if ( location.pathname.replace( /^\//, '' ) === this.pathname.replace( /^\//, '' ) && location.hostname === this.hostname ) {
-        var target = $( this.hash );
-        target = target.length ? target : $( '[name=" + this.hash.slice( 1 ) + "]' );
-        if ( target.length ) {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=" + this.hash.slice( 1 ) + "]');
+        if (target.length) {
 
-          $( 'html,body' ).animate( {
+          $('html,body').animate({
             scrollTop: target.offset().top
-          }, 1000 );
-          if ( matchMedia( 'only screen and (max-width: 1100px)' ).matches ) {
+          }, 1000);
+          if (matchMedia('only screen and (max-width: 1100px)').matches) {
 
-            $( 'html,body' ).animate( {
+            $('html,body').animate({
               scrollTop: target.offset().top - 60
-            }, 1000 );
+            }, 1000);
             return false;
 
           }
@@ -102,54 +102,54 @@ $( function() {
 
     }
 
-  } );
+  });
 
-} );
+});
 var offset = 300,
 
-    //Browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  //Browser window scroll (in pixels) after which the "back to top" link opacity is reduced
   offset_opacity = 1200,
 
-    //Duration of the top scrolling animation (in ms)
+  //Duration of the top scrolling animation (in ms)
   scroll_top_duration = 700,
 
-    //Grab the "back to top" link
-  $back_to_top = $( '.cd-top' );
+  //Grab the "back to top" link
+  $back_to_top = $('.cd-top');
 
 //Hide or show the "back to top" link
-$( window ).scroll( function() {
+$(window).scroll(function () {
 
-  ( $( this ).scrollTop() > offset ) ? $back_to_top.addClass( 'cd-is-visible' ) : $back_to_top.removeClass( 'cd-is-visible cd-fade-out' );
-  if ( $( this ).scrollTop() > offset_opacity ) {
+  ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+  if ($(this).scrollTop() > offset_opacity) {
 
-    $back_to_top.addClass( 'cd-fade-out' );
+    $back_to_top.addClass('cd-fade-out');
 
   }
 
-} );
+});
 
 //Smooth scroll to top
-$back_to_top.on( 'click', function( event ) {
+$back_to_top.on('click', function (event) {
 
   event.preventDefault();
-  $( 'body,html' ).animate( {
+  $('body,html').animate({
     scrollTop: 0
-  }, scroll_top_duration );
+  }, scroll_top_duration);
 
-} );
+});
 
 //Schedules
 var headertext = [],
-  headers = document.querySelectorAll( '#schedules th' ),
-  tablerows = document.querySelectorAll( '#schedules th' ),
-  tablebody = document.querySelector( '#schedules tbody' );
+  headers = document.querySelectorAll('#schedules th'),
+  tablerows = document.querySelectorAll('#schedules th'),
+  tablebody = document.querySelector('#schedules tbody');
 
-for ( var i = 0; i < headers.length; i++ ) {
+for (var i = 0; i < headers.length; i++) {
   var current = headers[i];
-  headertext.push( current.textContent.replace( /\r?\n|\r/, '' ) );
+  headertext.push(current.textContent.replace(/\r?\n|\r/, ''));
 }
-for ( var i = 0, row; row = tablebody.rows[i]; i++ ) {
-  for ( var j = 0, col; col = row.cells[j]; j++ ) {
-    col.setAttribute( 'data-th', headertext[j] );
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute('data-th', headertext[j]);
   }
 }
